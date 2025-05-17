2a603288ad6f0fb69eda78afb30a78b4a7c678c8
FValenzuelaB <164669852+FValenzuelaB@users.noreply.github.com>
2025-05-17 07:31:48 +0000

    se termina y testea juego.py

--- desafio2_SentenciasCondicionalesIterativas/juego.py
+++ desafio2_SentenciasCondicionalesIterativas/juego.py
@@ -3,17 +3,18 @@ import random
 import sys
 
 
-#Definir opciones y resultado computadora
-opciones={"piedra","papel","tijeras"}
-compu=random.choices(opciones)
-compu=compu[0]
+#Definir opciones
+opciones=["piedra","papel","tijeras","salir"]
 
-#Pedir a jugador su elección
 
 while True:
     try:
+        #Definir opcion de jugador y del compu para esta vuelta
         j=input('Elige piedra, papel o tijeras, si quieres detener el juego escribe "salir": ').lower()
+        compu=random.choices(opciones)
+        compu=compu[0]
 
+        #Establecer resultado para cada caso
         if j not in opciones:
             print('Invalido, debe ser "piedra", "papel" o "tijeras"')
             continue
@@ -22,4 +23,14 @@ while True:
             break
         elif j==compu:
             print('Empate!')
-        elif (j=="piedra" and compu=="tijeras") or (j==)
+        elif (j=="piedra" and compu=="tijeras") or (j=="papel" and compu=="piedra") or (j=="tijeras" and compu=="papel"):
+            print(f'Tu eligiste {j}, el computadór eligió {compu}:')
+            print('Ganaste!!')
+        else:
+            print(f'Tu eligiste {j}, el computadór eligió {compu}:')
+            print('Perdiste!')
+
+    #En caso de romperse el programa
+    except:
+        print("No can do")
+        break
