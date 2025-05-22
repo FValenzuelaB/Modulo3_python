5c9505db3216c10aafecff3e5819252c6014b72b
FValenzuelaB <164669852+FValenzuelaB@users.noreply.github.com>
2025-05-22 11:43:39 +0000

    se elimina variable innecesaria

--- desafio3_ScI_II/fuerza_bruta.py
+++ desafio3_ScI_II/fuerza_bruta.py
@@ -5,13 +5,11 @@ import getpass
 try:
     intentos=0
     password=getpass.getpass("Ingrese su clave:   ")
-    adivinadas=""
 
     for i in range(len(password)):
         for j in ascii_lowercase:
             intentos+=1
             if j==password[i]:
-                adivinadas+=j
                 break
     print(f"Se adivinó la pass: {password} en {intentos} intentos.")
 
