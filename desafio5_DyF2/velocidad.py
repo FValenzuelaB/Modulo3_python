55b5442c4be5ade1fe141599f7616b5b99b2eff7
FValenzuelaB <164669852+FValenzuelaB@users.noreply.github.com>
2025-05-31 05:52:18 +0000

    Desafio5 ejercicio 2 completo

---
+++ desafio5_DyF2/velocidad.py
@@ -0,0 +1,20 @@
+velocidad = [25, 12, 19, 16, 11, 11, 24, 1,
+14, 14, 16, 10, 6, 23, 13, 25, 4, 19,
+14, 20, 18, 9, 18, 4, 18, 1, 3, 4, 2,
+14, 23, 19, 23, 9, 18, 20, 22, 14, 1,
+10, 5, 23, 3, 5, 9, 5, 3, 12, 20, 5,
+11, 10, 18, 10, 14, 5, 23, 20, 23, 21]
+
+
+def correa_loca(velocidades:list[int]):
+    #Priemero se calcula el promedio (suma total hecha con el for)/(total de datos extraida de el largo de la lista)
+    mediciones=len(velocidades)
+    suma=0
+    for i in velocidad:
+        suma+=i
+    promedio=suma/mediciones
+    #La función procede a devolver una lista hecha mediante "List comprehension" syntax de la posición de las correas con valores mayores al promedio
+    return f"La posición de correas con velocidades sobre el promedio són:  \n{[i for i in range(len(velocidades)) if velocidades[i]>promedio]}"
+
+
+print(correa_loca(velocidad))
