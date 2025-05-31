c304aff330cc609a49235a2ef5b3eadaad898482
FValenzuelaB <164669852+FValenzuelaB@users.noreply.github.com>
2025-05-31 10:56:52 +0000

    desafio5 ejercicio 3 y todos, completos

--- desafio5_DyF2/ong.py
+++ desafio5_DyF2/ong.py
@@ -14,3 +14,14 @@ def productoria(numeros:list):
     return f"La productoria de {numeros} es {prod}"
 
 
+def calcular(**kwargs):
+    for k,v in kwargs.items():
+        if "fact_" in k:
+            print(factorial(v))
+        elif "prod_" in k:
+            print(productoria(v))
+        else:
+            raise SyntaxError("Debes ingresar el requerimiento con formato fact_1 = int, prod_1 = list[int o float], etc") 
+
+
+calcular(fact_1 = 5, prod_1 = [3,6,4,2,8], fact_2 = 6)
