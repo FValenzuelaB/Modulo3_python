ad73d70a74293222028f9125ebb6d9386c86cd2a
FValenzuelaB <164669852+FValenzuelaB@users.noreply.github.com>
2025-05-31 05:16:59 +0000

    desafio5 ejercicio 1 completo

---
+++ desafio5_DyF2/filtro.py
@@ -0,0 +1,35 @@
+import sys
+
+# Diccionario de artículos y precios en dólares
+precios = {
+    'Notebook': 700,
+    'Teclado': 30,
+    'Mouse': 12,
+    'Monitor': 250,
+    'Escritorio': 135,
+    'Tarjeta de Video': 1500
+}
+
+def filtro(precios: dict[str, int], umbral: int):
+    if len(sys.argv) >= 3:
+        criterio = str(sys.argv[2]).lower().strip()
+    else:
+        criterio = "mayor"
+
+    if criterio == "mayor":
+        return [k for k, v in precios.items() if v > umbral]
+    elif criterio == "menor":
+        return [k for k, v in precios.items() if v < umbral]
+    else:
+        raise ValueError(f"\nEl criterio ingresado es incorrecto: '{sys.argv[2]}'. Usa 'mayor' o 'menor'")
+
+try:
+    if len(sys.argv) < 2:
+        raise ValueError("Debes ingresar al menos un umbral de precio como argumento.")
+    
+    umbral = int(sys.argv[1])
+    resultado = filtro(precios, umbral)
+    print("Artículos filtrados:", resultado)
+
+except Exception as e:
+    print("Error:", e)
